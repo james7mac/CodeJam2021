73b168ff2561fa3133effb0b082f517a5b606b93
james7mac <32667561+james7mac@users.noreply.github.com>
2021-06-25 10:39:54 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, centered: bool = False) -> str:
+    """
+    :param rows: 2D list containing objects that have a single-line representation (via `str`).
+    All rows must be of the same length.
+    :param labels: List containing the column labels. If present, the length must equal to that of each row.
+    :param centered: If the items should be aligned to the center, else they are left aligned.
+    :return: A table representing the rows passed in.
+    """
+    
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
